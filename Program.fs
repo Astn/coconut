370c65601498b2e03a260cf40b2eed0523a6bcbc
Austin Harris <astn@Austins-MacBook-Pro.local>
2015-05-06 16:57:08 -0600

    spelling

--- Program.fs
+++ Program.fs
@@ -1,30 +1,30 @@
 ﻿[<EntryPoint>]
 let main argv = 
-    let saliors = 5
+    let sailors = 5
 
     let sneak (coconuts, pile, _) = 
-        let piles = pile / saliors
-        let leftovers = pile % saliors
-        (coconuts, piles * (saliors - 1), leftovers)
+        let piles = pile / sailors
+        let leftovers = pile % sailors
+        (coconuts, piles * (sailors - 1), leftovers)
 
     let happyMonkey (coconuts, pile, leftovers) = leftovers = 1
 
-    let saliorSneakAndMonkey coconuts = 
+    let sailorsneakAndMonkey coconuts = 
         coconuts
         |> Seq.map sneak
         |> Seq.filter happyMonkey
 
     let viablePileSizes = 
-        {saliors .. System.Int32.MaxValue}
+        {sailors .. System.Int32.MaxValue}
         |> Seq.map (fun coconut -> (coconut, coconut, 0))
-        |> saliorSneakAndMonkey
-        |> saliorSneakAndMonkey
-        |> saliorSneakAndMonkey
-        |> saliorSneakAndMonkey
-        |> saliorSneakAndMonkey
+        |> sailorsneakAndMonkey
+        |> sailorsneakAndMonkey
+        |> sailorsneakAndMonkey
+        |> sailorsneakAndMonkey
+        |> sailorsneakAndMonkey
         |> Seq.filter (fun (coconuts, pile, leftovers) -> pile % 5 = 0)
         |> Seq.map (fun (coconuts, _, _) -> coconuts)
-        |> Seq.take 5
+        |> Seq.take 10
 
     printfn "Piles could be : %A" viablePileSizes
     0 // return an integer exit code
